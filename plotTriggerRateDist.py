8e3900195f68cd55373d804dcb67d77d47de7f3e
Matthew Marsee <85581853+MatthewMarsee@users.noreply.github.com>
2025-04-25 21:46:23 -0500

    Initial Upload

---
+++ plotTriggerRateDist.py
@@ -0,0 +1,54 @@
+import argparse
+from procfunc import *
+from rnog_data.runtable import RunTable #***YOU MAY NEED TO CHANGE THIS***
+
+
+runType_labels = ['physics','calibration','other']
+runtable = RunTable().get_table()
+
+#=========#
+
+#Parameters
+parser = OptionParser("usage: %prog [options] inputfile")
+parser.add_option("-s", "--station", default="11",dest="station_id", help="Station number")
+(options,args) = parser.parse_args()
+station = options.station_id
+
+
+#==========#
+
+data = getData(station, f'runStatsdata_st{station}_2021-01-01to2025-01-01.csv')
+#time = data[0]                      #timestamps
+run_ids = data[1].astype(int)       #run numbers
+trigRate = data[2]                  #trigger rates
+#run_dt = data[3]                    #run duration (s)
+#data_dt = data[4]                   #data taking duration (s)
+
+
+#plotHist(station,trigRate,'Trigger Rate','Hz',0.2)
+#=============================#
+
+run_types = [] #for comparison with complete dataset
+
+#for plotting in separate histograms
+sorted_runlists = [[] for t in runType_labels]
+sorted_triggers = [[] for t in runType_labels]
+
+for ir,run in enumerate(run_ids):
+    rt = getRunType(station,run,runtable)
+    run_types.append(rt)
+
+    if rt in runType_labels[:-1]:
+        sorted_runlists[runType_labels.index(rt)].append(run)
+        sorted_triggers[runType_labels.index(rt)].append(trigRate[ir])
+    else:
+        sorted_runlists[-1].append(run)
+        sorted_triggers[-1].append(trigRate[ir])
+
+
+#plotRunTypeHist(station,sorted_triggers,'Trigger Rate','Hz',0.2,runType_labels)  #Plot Physics, Calibration and other runs; for reference
+plotRunTypeHist(station,sorted_triggers,'Trigger Rate','Hz',0.2,runType_labels,physonly=True) #Only 'Physics' runs will be included in the analysis
+
+
+
+
---
+++ procfunc.py
@@ -0,0 +1,106 @@
+import numpy as np
+import csv
+import math
+
+import matplotlib.pyplot as plt
+plt.rcParams['axes.facecolor'] = 'white'
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 300
+
+stations = [11,12,13,14,21,22,23,24]
+colors = ['yellow','orange','red','magenta','lawngreen','cyan','blue','purple']
+
+
+#=======================================================#
+def getData(station,filename):
+    #> Access data in csv from grafana data grabber
+    data = []
+    with open(filename,'r') as rdf:
+        for i,line in enumerate(csv.reader(rdf)):
+            if i==0:
+                params = line
+                data = [[] for par in line]
+            else:
+                if float(line[[j for j,p in enumerate(params) if 'trigger rate' in p][0]]) <0: #ignore runs with trigger rates around -1000
+                    continue
+                for ip,p in enumerate(params):
+                    if 'run ids' in p:
+                        data[ip].append(int(float(line[ip])))
+                    else:
+                        data[ip].append(float(line[ip]))
+    return np.array(data)
+
+
+def getRunType(station,run,runtable):
+    #>Get run type from runtable
+    headers = ['run_type']
+
+    mask_station = (runtable['station'] == int(station))
+    mask_run = (runtable['run'] == int(run))
+    mask = mask_station * mask_run
+
+    res = runtable[mask][headers].values
+    if len(res)==0:
+        return 'MISSING'
+    else:
+        return res[0][0]
+
+
+
+#=============#
+
+def plotHist(station,param_data,param_name,param_unit,bin_width):
+    #>Plot trigger rates for all runs of a station 
+
+    plt.axvline(2,c='r',linestyle=':',label='2Hz')
+
+    bins = np.arange(math.floor(np.min(param_data)), math.ceil(np.max(param_data)) + bin_width, bin_width)
+    plt.hist(param_data,bins=bins,color=colors[stations.index(station)],label=f'N = {len(param_data)}\n  '+
+                                                                            r'$\mu$'+f' = {np.round(np.mean(param_data),3)}\n  '+
+                                                                            r'$\sigma$'+f' = {np.round(np.std(param_data),3)}'
+                                                                            )
+    plt.suptitle(f'{param_name}s per Run: Station {station}\n2021 - 2025')
+    plt.xlabel(f'{param_name} [{param_unit}]')
+    plt.ylabel('Count')
+    plt.yscale('log')
+    plt.legend()
+    filename =  f'{param_name.replace(" ","")}s_Station{station}.png'
+    plt.savefig(filename,dpi=300)
+    print(f'Plot saved to {filename}')
+    plt.show()
+
+
+def plotRunTypeHist(station,param_data,param_name,param_unit,bin_width,runtypes,physonly=False):
+    #>Plot trigger rates, sorting by trigger types.
+    #>Option to only show physics runs (no calibration or broken runs will be used in the analysis)
+
+    plt.axvline(2,c='r',linestyle=':',label=f'2 {param_unit} upper bound')
+    hatches = ['x','-','o']
+
+    counter=0
+    for k, rType in enumerate(runtypes):
+        if physonly and not (rType=='physics'):
+            continue
+        bins = np.arange(math.floor(np.min(param_data[k])), math.ceil(np.max(param_data[k])) + bin_width, bin_width)
+        plt.hist(param_data[k],hatch=hatches[k],alpha = 0.5,bins=bins,
+                                               label=f'{rType[0].capitalize()+rType[1:]}\n'+
+                                               f'  N = {len(param_data[k])}\n'+
+                                               r'  $\mu$'+f' = {np.round(np.mean(param_data[k]),3)}'
+                                               ) 
+        counter+=1
+
+    plt.suptitle(f'{param_name}s per Run: Station {station}\n2021 - 2025; By Run Type')
+    plt.xlabel(f'{param_name} [{param_unit}]')
+    plt.ylabel('Count')
+    plt.yscale('log')
+    plt.legend()
+    filename =  f'{param_name.replace(" ","")}s_Station{station}_{counter}runTypes.png'
+    plt.savefig(filename,dpi=300)
+    print(f'Plot saved to {filename}')
+    plt.show()
+
+
+#=================================#
+
+
+
